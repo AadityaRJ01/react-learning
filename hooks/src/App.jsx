560e2934144b9bbc841ac451f8778b83aaaff59f
AadityaRJ01 <1ms23cs002@msrit.edu>
2026-03-07 14:31:13 +0530

    useReducer Hook

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import StateExample from "./diif-hooks/useState";
+import { EffectExample } from "./diif-hooks/use-effect";
+import { ReducerExample } from "./diif-hooks/use-reducer";
+// import StateExample from "./diif-hooks/useState";
 
 
 function App() {
   return (
     <>
-      <StateExample/>
+      {/* <StateExample/> */}
+      {/* <EffectExample/> */}
+      <ReducerExample/>
     </>
   );
 }
